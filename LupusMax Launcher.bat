be793374054bdf2f568379a628fa78b1206bd9b0
Enobraed <70288107+Enobraed@users.noreply.github.com>
2025-08-11 12:15:42 +0300

    UPDATE 11.08.2025
    1. Добавлен вывод информации в консоль
    2. Добавлены новые файлы и папки на удаление

--- LupusMax Launcher.bat
+++ LupusMax Launcher.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 setlocal enabledelayedexpansion
 
 :: ============================================================
@@ -21,7 +22,7 @@ setlocal enabledelayedexpansion
 :: Лицензия:     MIT
 :: ============================================================
 
-:: Завершение всех процессов LibreWolf
+@echo 1. Завершение всех процессов LibreWolf
 taskkill /f /im LibreWolf-WinUpdater.exe >nul 2>&1
 taskkill /f /im LibreWolfPortable.exe >nul 2>&1
 taskkill /f /im librewolf.exe >nul 2>&1
@@ -31,57 +32,60 @@ set "base=%~dp0"
 set "librewolf=%base%LibreWolf"
 set "pf=%base%Profiles\Default"
 
-:: Бесшумное обновление LibreWolf
+@echo 2. Бесшумное обновление LibreWolf
 start "" /min /wait "%base%LibreWolf-WinUpdater.exe" --silent
 
-:: Завершение процесса обновления
+@echo 3. Завершение процесса обновления
 taskkill /f /im LibreWolf-WinUpdater.exe >nul 2>&1
 
-:: Удаление ненужных папок LibreWolf после обновления
+@echo 4. Удаление ненужных папок LibreWolf после обновления
 for %%d in (
     browser\VisualElements
     defaults
     fonts
     gmp-clearkey
+    installation_dir_layout
     uninstall
 ) do rd /s /q "%librewolf%\%%d" >nul 2>&1
 
-:: Удаление ненужных файлов LibreWolf после обновления
+@echo 5. Удаление ненужных файлов LibreWolf после обновления
 for %%f in (
     AccessibleMarshal.dll
     application.ini
+    dxcompiler.dll
     firefox.VisualElementsManifest.xml
+    InstallationDirLayout.dll
     libEGL.dll
     libGLESv2.dll
-    vcruntime140_1.dll
-    vcruntime140.dll
-    removed-files
-    private_browsing.VisualElementsManifest.xml
-    private_browsing.exe
-    precomplete
-    plugin-container.exe
-    platform.ini
-    pingsender.exe
-    notificationserver.dll
-    nmhproxy.exe
     msvcp140.dll
+    nmhproxy.exe
+    notificationserver.dll
+    pingsender.exe
+    platform.ini
+    plugin-container.exe
+    precomplete
+    private_browsing.exe
+    private_browsing.VisualElementsManifest.xml
+    removed-files
+    vcruntime140.dll
+    vcruntime140_1.dll
 ) do del /f /q "%librewolf%\%%f" >nul 2>&1
 
-:: Удаление мусора от WinUpdater после обновления
+@echo 6. Удаление мусора от WinUpdater после обновления
 del /f /q "%base%ScheduledTask-Create.ps1" "%base%ScheduledTask-Remove.ps1" >nul 2>&1
 
-:: Обновление файла policies.json
+@echo 7. Обновление файла policies.json
 if not exist "%librewolf%\distribution" mkdir "%librewolf%\distribution"
 curl -L -o "%librewolf%\distribution\policies.json" "https://raw.githubusercontent.com/Enobraed/LupusMax/main/policies.json" >nul 2>&1
 
-:: Обновление файла user.js
+@echo 8. Обновление файла user.js
 curl -L -o "%pf%\user.js" "https://raw.githubusercontent.com/Enobraed/LupusMax/main/user.js" >nul 2>&1
 
-:: Обновление файла userChrome.css
+@echo 9. Обновление файла userChrome.css
 if not exist "%pf%\chrome" mkdir "%pf%\chrome"
 curl -L -o "%pf%\chrome\userChrome.css" "https://raw.githubusercontent.com/Enobraed/LupusMax/main/userChrome.css" >nul 2>&1
 
-:: Очистка временных директорий профиля
+@echo 10. Очистка временных директорий профиля
 for %%d in (
     bookmarkbackups
     cache2
@@ -98,17 +102,19 @@ for %%d in (
     thumbnails
 ) do rd /s /q "%pf%\%%d" >nul 2>&1
 
-:: Очистка временных файлов сайтов
+@echo 11. Очистка временных файлов сайтов
 for /d %%i in ("%pf%\storage\default\https*") do rd /s /q "%%i" >nul 2>&1
 
-:: Удаление пустых папок
+@echo 12. Удаление пустых папок
 for /f "delims=" %%d in ('dir "%base%" /ad /b /s ^| sort /r') do rd "%%d" 2>nul
 
-:: Удаление пустых файлов
+@echo 13. Удаление пустых файлов
 for /r "%base%" %%f in (*) do if %%~zf==0 del /f /q "%%f" >nul 2>&1
 
-:: Запуск LibreWolf
+@echo 14. Запуск LibreWolf
 start "" "%base%LibreWolf-Portable.exe" -no-deelevate -purgecaches >nul 2>&1
+
+timeout /t "3" /nobreak >nul
 
 endlocal
 exit /b
